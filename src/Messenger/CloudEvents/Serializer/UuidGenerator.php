eeefb0540ab951ef5cdbd9ad0578ee7703dceb90
Rene Terstegen <rene@reneterstegen.eu>
2025-09-30 21:47:33 +0200

    Removed CloudEventFactory service reference

--- src/Messenger/CloudEvents/Serializer/UuidGenerator.php
+++ src/Messenger/CloudEvents/Serializer/UuidGenerator.php
@@ -9,7 +9,6 @@ readonly class UuidGenerator implements IdGeneratorInterface
 {
     public function __construct(private UuidFactoryInterface $uuidFactory) {}
 
-
     public function generate(Envelope $envelope): string
     {
         return $this->uuidFactory->uuid4()->toString();
